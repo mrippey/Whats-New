f7e25a9591f2e424b4da5b360f1c44505f8d8895
Mike R <30969281+msec1203@users.noreply.github.com>
2021-06-06 18:29:51 +0900

    Update whats_new_nix.py

--- whats_new_nix.py
+++ whats_new_nix.py
@@ -1,10 +1,5 @@
 __author__ = "Michael Rippey"
 __date__ = "2021/05/23"
-"""
-Author: Michael Rippey (c) 2021
-
-Copyright 2021 Michael Rippey
-"""
 
 from datetime import datetime 
 import pathlib
@@ -57,13 +52,12 @@ def from_new_nix_files(tree):
     time.sleep(2)
 
     for entry in tree.iterdir():
-        if entry.suffix in common_nix_extensions:
-           file_date = entry.stat()
-           dir_and_file_info = f"{entry.name:<25s} Last Modified: {check_file_time(file_date.st_birthtime):<12s}"
+        if entry.suffix in common_nix_extensions:    
+            file_date = entry.stat()
+            dir_and_file_info = f"{entry.name:<25s} \
+                Last Modified: {check_file_time(file_date.st_birthtime):<12s}"
            print(str(dir_and_file_info))
             
-
-           
 
     print("*" * 100)
     print("\n[+] Scan complete")
